33a18830d2016aef198b9646a359828fc7f939ea
thang <vuongquangthang0311@gmail.com>
2025-12-02 13:56:27 +0700

    fix 4

--- api/src/main/java/com/example/mindmap/features/mindmap/content/EdgeData.java
+++ api/src/main/java/com/example/mindmap/features/mindmap/content/EdgeData.java
@@ -1,13 +1,20 @@
 // src/main/java/com/example/mindmap/features/mindmap/content/EdgeData.java
 package com.example.mindmap.features.mindmap.content;
 
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
+
+@JsonIgnoreProperties(ignoreUnknown = true)
 public class EdgeData {
+    private String id;
     private String from;
     private String to;
-    
+
+    public String getId() { return id; }
+    public void setId(String id) { this.id = id; }
+
     public String getFrom() { return from; }
     public void setFrom(String from) { this.from = from; }
-    
+
     public String getTo() { return to; }
     public void setTo(String to) { this.to = to; }
 }
